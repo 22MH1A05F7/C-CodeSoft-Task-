4ae955e0f803d60c0e506fd71b5b2448f1eb7ce0
22MH1A05F7 <119723363+22MH1A05F7@users.noreply.github.com>
2023-11-16 22:17:43 +0530

    Simple Calculator.cpp

--- Simple Calculator.cpp
+++ Simple Calculator.cpp
@@ -3,30 +3,34 @@ using namespace std;
 int main() {
   char op;
   float number1, number2;
-  cout << "Enter operator: +, -, *, /: ➡️";
+  cout << "Enter operator: +, -, *, /: ";
   cin >> op;
-  cout << "Enter two operands: ➡️";
+  cout << "Enter two operands: ";
   cin >> number1 >> number2;
   switch(op) {
 
     case '+':
-      cout << number1 << " ➕ " << number2 << "  ⇒ " << number1 + number2;
+      cout << number1 << " + " << number2 << "  ⇒ " << number1 + number2;
       break;
 
     case '-':
-      cout << number1 << " ➖ " << number2 << "  ⇒ " << number1 - number2;
+      cout << number1 << " - " << number2 << "  ⇒ " << number1 - number2;
       break;
 
     case '*':
-      cout << number1 << " ✖️ " << number2 << " ⇒ " << number1 * number2;
+      cout << number1 << " x " << number2 << " ⇒ " << number1 * number2;
       break;
 
     case '/':
-      cout << number1 << " ➗ " << number2 << "  ⇒ " << number1 / number2;
+      cout << number1 << " / " << number2 << "  ⇒ " << number1 / number2;
+      break;
+
+    case '%':
+      cout << number1 << " % " << number2 << "  ⇒ " << number1 / number2;
       break;
       
     default:
-      cout <<"❌ Invalid Operator ❌"; 
+      cout <<" Invalid Operator "; 
       break;
   }
   return 0;
